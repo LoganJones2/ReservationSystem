2c12f78840bf943f6746628aef60fd47af2ed014
Dan Hill <dan@danhill.us>
2014-04-04 23:43:35 -0500

    implemented passenger constructor

--- ReservationSystem/Passenger.cpp
+++ ReservationSystem/Passenger.cpp
@@ -1,8 +1,18 @@
 #include "Passenger.h"
 
 
-Passenger::Passenger(int, int, std::string, std::string, int, int)
+Passenger::Passenger(int new_reservation_number, 
+	int new_seat_number, 
+	std::string new_first_name, 
+	std::string new_last_name, 
+	int new_menu_preference, 
+	int new_phone_number)
 {
+	seat_number = new_seat_number;
+	first_name = new_first_name;
+	last_name = new_last_name;
+	menu_preference = new_menu_preference;
+	phone_number = new_phone_number;
 }
 
 
